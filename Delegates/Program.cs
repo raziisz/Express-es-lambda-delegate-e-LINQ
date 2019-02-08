c6f2ed35d4da5efee8aa8b8f122f88ba0dbf8622
Luiz Felipe <raziel_libertino@hotmail.com>
2019-02-08 14:36:27 -0400

    Introducao delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using Delegates.Servicoes;
+using System;
+
+namespace Delegates
+{
+    delegate double OperacaoNumericaBinaria(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            OperacaoNumericaBinaria op = CalcularServico.Sum;
+
+            double resultado = op.Invoke(a, b);
+            Console.WriteLine(resultado);
+        }
+    }
+}
