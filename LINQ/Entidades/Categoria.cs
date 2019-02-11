8bb53d968eeb0922f44388ebfe4d7db0420b92d0
Luiz Felipe <raziel_libertino@hotmail.com>
2019-02-11 09:43:25 -0400

    Criado as classes produto e categoria na DEMO de LINQ

---
+++ LINQ/Entidades/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ.Entidades
+{
+    class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Tier { get; set; }
+
+        public Categoria(int id, string nome, int tier)
+        {
+            Id = id;
+            Nome = nome;
+            Tier = tier;
+        }
+    }
+}
---
+++ LINQ/Entidades/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ.Entidades
+{
+    class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public Categoria Categoria { get; set; }
+
+        public Produto(int id, string nome, double preco, Categoria categoria)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Categoria = categoria;
+        }
+    }
+}
