0c4d09dff02b37dc6502d9dfaee72aabc35ea0f0
Luiz Felipe <raziel_libertino@hotmail.com>
2019-02-11 09:36:16 -0400

    Introducao de LINQ

---
+++ LINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Especificar o data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Definira consulta expressão
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            //Executar a consulta
+            foreach (var x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
